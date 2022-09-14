e9cece8e0dddc965aceb4ff32d946546a08c9dd4
cesaraugusto316 <cesaraugusto316@protonmail.com>
2022-09-13 20:14:07 -0500

    adding state from branch dev

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,10 @@ import { App } from './App';
 import './index.css';
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+ReactDOM
+  .createRoot(document.getElementById('root') as HTMLElement)
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
